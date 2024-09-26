74318bdc9e7daf9ebc4a4b578b3b35556652814e
Swayamjeet <swayam.hp001@gmail.com>
2024-09-27 00:15:39 +0530

    Added executor for concurrent request handling

--- src/main/java/sockets/HttpServerSocket.java
+++ src/main/java/sockets/HttpServerSocket.java
@@ -10,25 +10,39 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServerSocket {
     private final ServerSocket serverSocket;
     private final int PORT = 4221;
+    ExecutorService executor;
 
     public HttpServerSocket() throws IOException {
         serverSocket = new ServerSocket(PORT);
         serverSocket.setReuseAddress(true);
+        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     }
 
-    public void listen() throws IOException {
+    public void listen() {
         while (true) {
-            Socket clientSocket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            HttpRequest request = HttpRequestParser.parse(in);
-            HttpResponse response = new HttpResponse(request);
-            OutputStream os = clientSocket.getOutputStream();
-            os.write(response.respond().getBytes());
-            os.close();
+            try {
+                Socket clientSocket = serverSocket.accept();
+                executor.execute(() -> {
+                    try {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        HttpRequest request = HttpRequestParser.parse(in);
+                        HttpResponse response = new HttpResponse(request);
+                        OutputStream os = clientSocket.getOutputStream();
+                        os.write(response.respond().getBytes());
+                        os.close();
+                    } catch (IOException e) {
+                        System.out.println("Error handling request: " + e.getMessage());
+                    }
+                });
+            } catch (IOException e) {
+                System.out.println("Error accepting client connection: " + e.getMessage());
+            }
         }
     }
 }
